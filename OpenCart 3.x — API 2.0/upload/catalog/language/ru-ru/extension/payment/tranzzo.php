5d38b6ffc3112b54c47194ef8c34c9dea87ab7e8
pavluk <info@art-pavluk.com>
2019-10-05 09:55:44 +0300

    add module opencart 3

---
+++ OpenCart 3.x — API 2.0/upload/catalog/language/ru-ru/extension/payment/tranzzo.php
@@ -0,0 +1,16 @@
+<?php
+// Text
+$_['text_title']		= 'Оплата через TRANZZO';
+$_['text_credit_card']	= 'Подтверждение платежа';
+$_['text_loading']		= 'Загрузка...';
+$_['text_pay_success']		= 'Заказ был успешно оплачен через TRANZZO. На сумму %1$s';
+//new
+$_['text_pay_auth']		= 'Платеж был успешно заблокирован через TRANZZO. Для зачисления платежа измените статус заказа на ';
+$_['text_pay_void']		= 'Платеж был успешно разблокирован через TRANZZO';
+//new
+$_['text_payment_id']		= 'ID платежа(payment id)';
+//new
+//$_['text_transaction']		= 'ID транзакции(transaction id)';
+$_['text_order']		= 'ID транзакции(order id)';
+//new
+$_['text_pay_refund']		= 'Заказ был успешно возвращен через TRANZZO';
